9ea5d0bbace6d00aa9101c7c2e7088b793fd110f
Shura <trunghieuit191@gmail.com>
2017-12-13 11:55:22 +0700

    1.0.5

--- src/controller/ManagerTaiKhoan.java
+++ src/controller/ManagerTaiKhoan.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import Dao.TaiKhoanDao;
-import Dao.Validation;
+import model.Validation;
 
 /**
  * Servlet implementation class ManagerTaiKhoan
--- src/model/Validation.java
+++ src/model/Validation.java
@@ -1,4 +1,4 @@
-package Dao;
+package model;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
